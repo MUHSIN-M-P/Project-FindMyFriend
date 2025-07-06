cbd27282ed5c83d605a4829a3da2d21050438275
MUHSIN-M-P <18muhsinmp@gmail.com>
2025-07-06 21:41:14 +0530

    frontend repo adjust

---
+++ frontend/next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:5000/:path*"
+            : "/api/:path*",
+      },
+    ];
+  },
+};
+
+export default nextConfig;
