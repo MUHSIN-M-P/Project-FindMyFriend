8a2ab6c2e570c24bf8a70243ce607470149b138a
Rahan-M <rahan01729@gmail.com>
2025-07-09 12:08:37 +0530

    feat: create navbar file, add it to layout.tsx

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const Navbar=()=>{
+    return(
+        <div className="bg-background">
+            <div className="main_title">
+                Find Your Tribe
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
